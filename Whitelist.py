a2decda30fcde229e5c40328a31b5459962acf74
Forvard <ficaliighvdrb@gmail.com>
2023-08-07 14:16:47 +0300

    0.0.1

--- Whitelist.py
+++ Whitelist.py
@@ -14,7 +14,8 @@ logger.add(stderr,
 
 def wallet():
     with open('wallet.txt', 'r') as file:
-        return file.read().splitlines()
+        file = file.read().splitlines()
+        return [address.replace(",", "").replace(";", "") for address in file]
 
 
 def check_whitelist(session: Session, chain_id: str, sub_id: int) -> list:
@@ -46,45 +47,70 @@ def add_whitelist(chain: str) -> None:
                     'fantom': 1782, 'ethereum': 2, 'core': 2806, 'bsc': 1896}
 
     addresses = check_whitelist(session, chain_id[chain], chain_id_sub[chain])
+    x = lambda a: a // -20 * -1
+
     if addresses:
-        json_data = {
-            'chooseChain': True,
-            'formGroupIndexes': [i for i in range(len(addresses))],
-            'authFlag': True,
-            'subCurrencyId': chain_id_sub[chain],
-            'generalType': 1,
-            'targetType': -1,
-            'currencyId': str(chain_id[chain]),
-            'addressInfoList': [{'address': adr, 'validateName': f'addressInfoList.{i}.address'}for i, adr
-                                in enumerate(addresses)],
-            'includeAuth': True,
-            'validateOnly': True,
-        }
-
-        session.post(
-            f'https://www.okx.com/v2/asset/withdraw/addressBatch?t={int(time() * 1000)}', json=json_data)
-        sleep(1)
-
-        session.get(f"https://www.okx.com/v2/asset/withdraw/add/address/sendEmailCode?t={int(time() * 1000)}&addressStr="
-                    f"{','.join(addresses)}&includeAuth=true")
-        sleep(5)
-        json_data_end = json_data.copy()
-        json_data_end['validateOnly'] = False
-        json_data_end['_allow'] = True
-        json_data_end['emailCode'] = input('Email code: ')
-        json_data_end['totpCode'] = input('Google auth: ')
-
-        r = session.post(
-            f'https://www.okx.com/v2/asset/withdraw/addressBatch?t={int(time() * 1000)}', json=json_data_end)
-        try:
-            response = loads(r.text)
-            if response["error_code"] == '0':
-                logger.success(f'Добавлены адреса: {"; ".join(addresses)}')
+        completed = []
+        for i in range(x(len(addresses))):
+            start = i * 20
+            end = (i + 1) * 20 if (i + 1) * 20 < len(addresses) else 0
+            if end:
+                addresses_input = addresses[start:end]
             else:
-                logger.error(f'{r.text}')
-        except BaseException as error:
-            logger.error(f'response : {r.text}')
-            logger.error(f'error: {error}')
+                addresses_input = addresses[start:]
+            json_data = {
+                'chooseChain': True,
+                'formGroupIndexes': [i for i in range(len(addresses_input))],
+                'authFlag': True,
+                'subCurrencyId': chain_id_sub[chain],
+                'generalType': 1,
+                'targetType': -1,
+                'currencyId': str(chain_id[chain]),
+                'addressInfoList': [{'address': adr, 'validateName': f'addressInfoList.{i}.address'}for i, adr
+                                    in enumerate(addresses_input)],
+                'includeAuth': True,
+                'validateOnly': True,
+            }
+
+            r = session.post(
+                f'https://www.okx.com/v2/asset/withdraw/addressBatch?t={int(time() * 1000)}', json=json_data)
+            try:
+                response = loads(r.text)
+                if response["error_code"] == '0':
+                    logger.success(f'Эмайл код отправлен')
+                else:
+                    logger.error(f'{r.text}')
+            except BaseException as error:
+                logger.error(f'response : {r.text}')
+                logger.error(f'error: {error}')
+            sleep(1)
+
+            session.get(f"https://www.okx.com/v2/asset/withdraw/add/address/sendEmailCode?t={int(time() * 1000)}&addressStr="
+                        f"{','.join(addresses_input)}&includeAuth=true")
+            sleep(5)
+            json_data_end = json_data.copy()
+            json_data_end['validateOnly'] = False
+            json_data_end['_allow'] = True
+            json_data_end['emailCode'] = input('Email code: ')
+            json_data_end['totpCode'] = input('Google auth: ')
+
+            r = session.post(
+                f'https://www.okx.com/v2/asset/withdraw/addressBatch?t={int(time() * 1000)}', json=json_data_end)
+            try:
+                response = loads(r.text)
+                if response["error_code"] == '0':
+                    logger.success(f'Добавлены адреса: {"; ".join(addresses_input)}')
+                    completed += addresses_input
+                    logger.success(f'{len(completed)} / {len(addresses)}')
+                    sleep(1)
+                    if len(completed) != len(addresses):
+                        input("Для дальнейшей раюоты нажми Enter")
+
+                else:
+                    logger.error(f'{r.text}')
+            except BaseException as error:
+                logger.error(f'response : {r.text}')
+                logger.error(f'error: {error}')
     else:
         logger.success('Все адреса уже были в вайтлисте')
 
